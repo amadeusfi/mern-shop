58bc40eca528a7b8f59c915e536d51803cb71f00
amadeusfi <amadeus_figueiredo@protonmail.com>
2021-10-20 15:24:19 +0200

    update ShippingScreen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Button } from "react-bootstrap"
+import {Form, Button, FormGroup, FormLabel} from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
@@ -11,10 +11,58 @@ const ShippingScreen = ({ history }) => {
     const [postalCode, setPostalCode] = useState('')
     const [country, setCountry] = useState('')
 
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submit')
+    }
+
     return (
-        <div>
-            Shipping
-        </div>
+        <FormContainer>
+            <h1>Shipping</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='address'>
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='enter address'
+                        value={address}
+                        required
+                        onChange={(e) => setAddress(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='city'>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='enter city'
+                        value={city}
+                        required
+                        onChange={(e) => setCity(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='postalCode'>
+                    <Form.Label>Postal Code</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='enter postal code'
+                        value={postalCode}
+                        required
+                        onChange={(e) => setPostalCode(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='country'>
+                    <FormLabel>Country</FormLabel>
+                    <Form.Control
+                        type='text'
+                        placeholder='enter country'
+                        value={country}
+                        required
+                        onChange={(e) => setCountry(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Button type='submit' variant='primary'>Continue</Button>
+            </Form>
+        </FormContainer>
     )
 }
 
